b00b75c8cb1c4a441aef9c26fe356c19b2ff07b7
jin <jiinliin@gmail.com>
2024-01-08 14:23:05 +0700

    done exercise

--- exercises/06_move_semantics/move_semantics6.rs
+++ exercises/06_move_semantics/move_semantics6.rs
@@ -5,14 +5,16 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// I AM  DONE
 
 fn main() { 
     let data = "Rust is great!".to_string();
 
     let c = get_char(&data);
     println!("cc = {}", c);
-    string_uppercase(&data);
+   let data2 =  string_uppercase(&data);
+    println!("data = {}", data);
+    println!("data = {:?}",data2);
 }
 
 // Should not take ownership
@@ -21,8 +23,9 @@ fn get_char(data: &String) -> char {
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
+fn string_uppercase( data: &String) -> String{
     let data = data.to_uppercase();
 
     println!("{}", data);
+    return data;
 }
